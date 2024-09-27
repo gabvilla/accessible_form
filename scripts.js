154fe81fbf82027a724b57252af08c1e2272b6ce
gabvilla <gabriel.villassanti2@hotmail.com>
2024-09-26 21:26:13 -0300

    main changes in the inputs and buttons styles

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const nameInput = document.querySelector('#name').value;
+const emaiInput = document.querySelector('#emai').value;
+const passwordInput = document.querySelector('#password').value;
+const passConfirmInput = document.querySelector('#password-confirm').value;
+const submitButton = document.querySelector('#submit');
+
+submitButton.addEventListener((''), )
+
+
